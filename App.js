c126ad2aeaa7425cda0da44c2f3b376b964ad448
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-08-03 20:15:59 +0530

    add button with the text Click Me

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button,Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
       <StatusBar style="auto" />
+      <Button title='Click ME'></Button>
     </View>
   );
 }
