4c0fb280d8a3f90a565648284dd32eac912eb2a4
Jilu Elizebeth <elizebeth18.jilu@gmail.com>
2025-08-04 21:55:40 +0530

    add textInput to create a goal and add it into the list of goals

--- App.js
+++ App.js
@@ -1,21 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Button,Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Button, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [goalText, setGoalText] = useState('');
+  const [goalsList, setGoalsList] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setGoalText(enteredText);
+  }
+
+  const addGoalHandler = () => {
+    setGoalsList(curGoalsList => [...curGoalsList, goalText]);
+    setGoalText("");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-      <Button title='Click ME'></Button>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} value={goalText} 
+          placeholder='Your course goal'
+          onChangeText={goalInputHandler} />
+
+        <Button title='Add Goal' onPress={addGoalHandler} />
+
+      </View>
+      <View style={styles.goalContainer}>
+        <Text>List of Goals</Text>
+        {goalsList.map((goal, index) => <Text key={index}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 16
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  textInput: {
+    width: '70%',
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    marginRight: 8,
+    padding: 8
+  },
+  goalContainer: {
+    flex: 4
+  }
 });
